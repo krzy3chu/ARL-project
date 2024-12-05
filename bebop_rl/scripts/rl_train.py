596f90dd686f7ffbb854bcdab95e2fd5949659d3
krzy3chu <krzysztof.witkowski.16@gmail.com>
2024-12-05 14:02:01 +0100

    feat: add basic utilities to bebop_rl package

--- bebop_rl/scripts/rl_train.py
+++ bebop_rl/scripts/rl_train.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 
 import rospy
+from std_srvs.srv import Empty
 from std_msgs.msg import String
+from mav_msgs.msg import Actuators
 
-def run():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('rl_train', anonymous=True)
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+class RLTrain:
+    def __init__(self):
+        rospy.init_node('rl_train', anonymous=True)
+        
+        self.rate = rospy.Rate(0.2)
+        
+        self.motor_cmd_pub = rospy.Publisher('/bebop/command/motors', Actuators, queue_size=10)
+        self.reset_world_service = rospy.ServiceProxy('/gazebo/reset_world', Empty)
+        self.reset_world_service.wait_for_service()
+
+    def run(self):
+        while not rospy.is_shutdown():
+            motor_cmd = Actuators()
+            motor_cmd.angular_velocities = [400, 400, 400, 400]
+            self.motor_cmd_pub.publish(motor_cmd)
+            rospy.loginfo("Publishing motor command: {}".format(motor_cmd.angular_velocities))
+            
+            self.reset_world_service()
+            rospy.loginfo("Resetting world")
+            
+            self.rate.sleep()
+
 
 if __name__ == '__main__':
     try:
-        run()
+        rltrain = RLTrain()
+        rltrain.run()
     except rospy.ROSInterruptException:
         pass
